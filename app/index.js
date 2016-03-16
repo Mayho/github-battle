34513a319188a91a9c19f11ac5a6490a60453161
Mehul Patel <mehul8@gmail.com>
2016-03-16 15:58:45 -0700

    🍩

--- app/index.js
+++ app/index.js
@@ -7,34 +7,45 @@ var USER_DATA = {
   image: 'https://avatars1.githubusercontent.com/u/3915230?v=3&s=460'
 };
 
-var App = React.createClass({
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function() {
     return (
       <div>
-        <Header />
-        <Content />
+        <a href={'https://www.github.com' + this.props.username}>
+          {this.props.username}
+        </a>
       </div>
     );
   }
 });
 
-var Header = React.createClass({
+var ProfileName = React.createClass({
   render: function() {
     return (
-      <h1>Steezy Traplords Unlimited</h1>
+      <div>{this.props.name}</div>
     );
   }
 });
 
-var Content = React.createClass({
+var Avatar = React.createClass({
   render: function() {
-      return (
-        <img src={USER_DATA.image} />
-      );
+    return (
+      <div>
+        <ProfilePic imgageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
   }
 });
 
 ReactDOM.render(
-  <App />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
