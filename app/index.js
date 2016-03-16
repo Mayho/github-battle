b39fed90b3b41482511f87ac7848bc19b3da5e08
Mehul Patel <mehul8@gmail.com>
2016-03-16 15:19:50 -0700

    💀

--- app/index.js
+++ app/index.js
@@ -1,15 +1,40 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+  name: 'Traplord',
+  username: 'Mayho',
+  image: 'https://avatars1.githubusercontent.com/u/3915230?v=3&s=460'
+};
+
+var App = React.createClass({
   render: function() {
     return (
-      <div>Trill AF!</div>
-    )
+      <div>
+        <Header />
+        <Content />
+      </div>
+    );
+  }
+});
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <h1>Steezy Traplords Unlimited</h1>
+    );
+  }
+});
+
+var Content = React.createClass({
+  render: function() {
+      return (
+        <img src={USER_DATA.image} />
+      );
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <App />,
   document.getElementById('app')
 );
