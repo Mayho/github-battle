53738dfa9a8321f61976ed90d8d3533ba7abd302
Mehul Patel <mehul8@gmail.com>
2016-07-27 17:54:58 -0700

    wire up click handlers and routing for results and restarting

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,4 +1,7 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
 
 function pre(object) {
   return <pre> {JSON.stringify(object, null, 2)} </pre>
@@ -7,7 +10,39 @@ function pre(object) {
 function ConfirmBattle(props) {
   return props.isLoading
     ? <p> It's loading fam! </p>
-    : <div> Confirm Battle: {pre(props)} </div>
+    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <div className='col-sm-6'>
+            <p className='lead'>Player One</p>
+            {pre(props.playersInfo[0])}
+          </div>
+          <div className='col-sm-6'>
+            <p className='lead'>Player Two</p>
+            {pre(props.playersInfo[1])}
+          </div>
+        </div>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <div className='col-sm-12' style={styles.space}>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
+              FIGHT!
+            </button>
+          </div>
+          <div className='col-sm-12' style={styles.space}>
+            <Link to='/playerOne'>
+              <button type='button' className='btn btn-lg btn-danger'>
+                Reselect Players
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.PropTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired
 }
 
 module.exports = ConfirmBattle;
